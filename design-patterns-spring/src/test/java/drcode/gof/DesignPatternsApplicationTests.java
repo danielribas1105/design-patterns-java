ede753dd6cc12aaca796b25d6570ca046827b50c
Daniel <danielribas1105@gmail.com>
2023-04-27 19:29:44 -0300

    create project spring

---
+++ design-patterns-spring/src/test/java/drcode/gof/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package drcode.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
